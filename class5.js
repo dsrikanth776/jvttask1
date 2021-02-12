e735ddc57798da2e16fa937d6a3217a5bfd961bc
dsrikanth776 <75516370+dsrikanth776@users.noreply.github.com>
2021-02-12 16:50:52 +0530

    Add files via upload

---
+++ class5.js
@@ -0,0 +1,176 @@
+// object            object              varable             data
+//  hr                student           goodmorning          goodmorning
+//                                      tellmeyourself        iamsrikanthfromap
+//                                       ruexperience         no iam a fresher
+//                                        whichyearpassout     2020
+//                                         expsalary          asperthecompenypolicy
+//type 1
+var goodmorning="goodmorning"
+var tellmeyourself="iamsrikanthfromap"
+var ruexperience="no iam a fresher"
+var whichyearpassout=2020
+var expsalary="asperthecompenypolicy"
+console.log(goodmorning,tellmeyourself,'<br>',ruexperience,'<br>',whichyearpassout,'<br>',expsalary);
+//type2
+var goodmorning="goodmorning"
+var tellmeyourself="iamsrikanthfromap"
+var ruexperience="no iam a fresher"
+var whichyearpassout=2020
+var expsalary="asperthecompenypolicy"
+console.log( goodmorning,tellmeyourself);
+
+//array 
+  var  names=["kishore","bharath","sreekanth","ramana"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[1] + names[2]);
+
+function interview(){
+    var goodmorning="goodmorning"
+    var tellmeyourself="iamsrikanthfromap"
+    var ruexperience="no iam a fresher"
+    var whichyearpassout=2020
+    var expsalary="asperthecompenypolicy"
+    // I taken line no 8 (any line take )
+    if(tellmeyourself=="iamsaifromap")
+    //its (if condition)
+    {
+        console.log("yes im srikanth completed interview" +" but not shotlist" )
+        return //line 18
+    }
+    var rushotlist="yes i shotlist next round"
+    var package=250000
+
+    console.log("yes im sai got offerletter" +" yesterday night")
+    return // endline  25 
+
+}
+interview()//line 8
+
+function interview1(){
+    var goodmorning="goodmorning"
+    var tellmeyourself="iamsrikanthfromap"
+    var rufresher="yesiam a fresher "
+    var whichyearpassout=2020
+    var expsalary="asperthecompenypolicy"
+    // I taken line no 55 (any line take )
+    if(rufresher=="yesiam a fresher ")//true
+    //its (if condition)
+    {
+        console.log(" iam a fresher" )//its statement
+        return//65
+
+    }
+        var ruexperience="yes i have experience"
+        var currentctc="per month 20k"
+         console.log("yes i have 5year experience")
+        return //70 
+}
+interview1()//line 52
+//     object                 object               varible           data
+//      srikanth             kishore               uhavezomatoapp    yes i have
+//                                                 plsorderpizza     veg or non-veg
+//                                                addone7up          ok sure
+//                                               totalprice           261
+
+function orderfood(){
+
+    var uhavezomatoapp="yes i have"
+
+    var plsorderpizza="veg or non-veg"
+
+    var addone7up="ok sure"
+    var totalprice=261
+   sreekanth(uhavezomatoapp,plsorderpizza,addone7up,totalprice)
+
+    console.log(totalprice)
+
+    }
+
+    function sreekanth(uhavezomatoapp,plsorderpizza,addone7up,totalprice){
+
+    if(plsorderpizza=="veg or non-veg"){
+
+        console.log("yes i ordered pizza");
+        return 
+
+    }
+
+    else  
+
+    {
+        console.log("no ordered pizza")
+    }
+
+    }
+
+    orderfood()
+
+    //     object                 object               varible           data
+//      srikanth             kishore               uhavezomatoapp    yes i have
+//                                                 plsorderpizza     veg or non-veg
+//                                                addone7up          ok sure
+//                                               totalprice           261
+
+function orderpizza(){
+
+    var uhavezomatoapp="yes i have"
+
+    var plsorderpizza="vegor non-veg"
+
+    var addone7up="ok sure"
+    var totalprice=261
+   sreekanth(uhavezomatoapp,plsorderpizza,addone7up,totalprice)
+
+    console.log(totalprice)
+
+    }
+
+    function sreekanth(uhavezomatoapp,plsorderpizza,addone7up,totalprice){
+
+    if(plsorderpizza=="vegornon-veg"){
+
+        console.log("yes i ordered pizza");
+        return 
+
+    }
+
+    else  
+
+    {
+        console.log("no ordered pizza")
+    }
+
+    }
+
+    orderpizza()
+
+//using function in side function 
+function shopping(){
+    var offline=["shirt","pant","mobile","gift"];
+    console.log(offline[2]);
+}
+shopping()
+
+function shop(){
+    var buy=["shirt","pant","mobile","gift"];
+    buy[0]="dhoti"
+    console.log()
+}
+shop()
+// var  
+// there using date variable 
+
+var a=Date();
+console.log(a);//thisplay today date time 
+// previcus century
+var d=new Date(99,11,24)
+console.log(d);
+
+//date adding string 
+var d= new Date("july 20,2021 11:30:00")
+console.log(d);
+ 
+
